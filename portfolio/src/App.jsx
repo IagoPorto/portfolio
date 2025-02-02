fbc9b600ac9b2f15fc6600894cde3b23c5a504da
IagoPorto <iagopm8@gmail.com>
2025-02-02 08:15:34 +0100

    add noise background

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -5,16 +5,18 @@ import { About } from './components/About.jsx';
 import { Experience } from './components/Experience.jsx';
 import { Projects } from './components/Projects.jsx';
 import './App.css';
+import NoiseBackground from './components/NoiseBackground.jsx';
 
 function App() {
   return (
     <main>
       <MouseFollower />
-      <aside class="left-section">
+      <NoiseBackground />
+      <aside className="left-section">
         <Presentation />
         <Menu />
       </aside>
-      <section class ="right-section">
+      <section className ="right-section">
         <About />
         <Experience />
         <Projects />
