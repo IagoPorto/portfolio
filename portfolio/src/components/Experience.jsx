d15bdaf63a223d6f7f17dbe6d65ecc47771ad870
Iago Porto <iagopm8@gmail.com>
2025-05-26 21:59:49 +0200

    Experience section done

--- portfolio/src/components/Experience.jsx
+++ portfolio/src/components/Experience.jsx
@@ -1,15 +1,20 @@
+import './Experience.css'
+
 export function Experience({ companyName, companyUrl, position, initDate, finalDate, details }) {
-    return(
-         <div className="job">
-            <h3>
-                {position} at <a>{companyName}</a>
-            </h3>
-            <p>{initDate} – {finalDate}</p>
-            <ul>
-                {details.map((item, index) => (
-                    <li key={index}>{item}</li>
-                ))}
-            </ul>
-        </div>
-    );
+  return (
+    <div className="job">
+      <h3>
+        {position} at{' '}
+        <a href={companyUrl} target="_blank" rel="noopener noreferrer">
+          {companyName}
+        </a>
+      </h3>
+      <p>{initDate} - {finalDate}</p>
+      <ul>
+        {details.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
