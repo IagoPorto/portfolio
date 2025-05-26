00af4d88bc0ee474dd6e19d26b90f42eb4e607ba
Iago Porto <iagopm8@gmail.com>
2025-05-26 22:20:02 +0200

    Project section taking form

--- portfolio/src/components/Project.jsx
+++ portfolio/src/components/Project.jsx
@@ -1,10 +1,16 @@
-export function Project () {
-    return(
-        <div className="project">
-            <h3>[Nombre del Proyecto]</h3>
-            <p><strong>Descripción:</strong> Breve descripción del proyecto, sus objetivos y resultados.</p>
-            <p><strong>Tecnologías utilizadas:</strong> React, Node.js, MongoDB, etc.</p>
-            <p><strong>Enlace:</strong> <a href="[URL del proyecto]" target="_blank" rel="noopener noreferrer">[URL del proyecto]</a></p>
-        </div>
-    );
+export function Project({ name, description, url, photo, tecnologies }) {
+  return (
+    <div className="project">
+      <h3>{name}</h3>
+      <p><strong>Description:</strong> {description}</p>
+      <p><strong>Stack:</strong> {tecnologies.join(', ')}</p>
+      <a href={url} target="_blank" rel="noopener noreferrer">
+        <img
+          src={photo}
+          alt={`${name} logo`}
+          className="project-photo"
+        />
+      </a>
+    </div>
+  )
 }
--- portfolio/src/components/ProjectsSection.jsx
+++ portfolio/src/components/ProjectsSection.jsx
@@ -5,9 +5,26 @@ export function ProjectsSection() {
     return (
         <section className="projects" id="projects">
             <h2 className="section-title">Personal Projects</h2>
-            <Project />
-            <Project />
-            <Project />
+            <Project
+                name="AutoGuard"
+                description="Android app that detects driver distractions and drowsiness in real time using MediaPipe facemesh and MPAndroidChart for visualization."
+                url="https://github.com/IagoPorto/autoguard"
+                photo="https://raw.githubusercontent.com/IagoPorto/IagoPorto/main/pictures/logo-completo-color.jpg"
+                tecnologies={["MediaPipe","MPAndroidChart","Java","Android SDK"]}
+            />
+
+
+
+            <Project
+            name="Drone-Firefly"
+            description="Embedded real-time drone system built on Raspberry Pi with Navio board. Modular architecture for motors, controller, PID algorithms, and sensors using RTIMULib (Kalman filter), with remote communication over Zenoh."
+            url="https://github.com/IagoPorto/Dron-Firefly"
+            photo="https://raw.githubusercontent.com/IagoPorto/Dron-Firefly/main/dron2.png"
+            tecnologies={[
+                "C++", "Raspberry Pi", "Navio", "RTIMULib", "Zenoh", "PID Control"
+            ]}
+            />
+
         </section>
     );
 }
