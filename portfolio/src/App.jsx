79490fd1b6b161a943b61d42b16b0f23c7abd8a3
Iago Porto <iagopm8@gmail.com>
2026-08-09 12:10:55 +0200

    perf: split 3D globe into a lazy chunk

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,15 +1,16 @@
-import { useEffect } from "react";
+import { lazy, Suspense, useEffect } from "react";
 import { MouseFollower } from "./components/MouseFollower.jsx";
 import { Presentation } from "./components/Presentation.jsx";
 import { Menu } from "./components/Menu.jsx";
 import { About } from "./components/About.jsx";
 import { ExperienceSection } from "./components/ExperienceSection.jsx";
 import { ProjectsSection } from "./components/ProjectsSection.jsx";
-import Globe3D from "./components/Globe3D.jsx";
 import ContactIcons from "./components/ContactIcons.jsx";
 import Footer from "./components/Footer.jsx";
 import "./App.css";
 import NoiseBackground from "./components/NoiseBackground.jsx";
+
+const Globe3D = lazy(() => import("./components/Globe3D.jsx"));
 
 function App() {
   useEffect(() => {
@@ -33,16 +34,21 @@ function App() {
 
   return (
     <>
+      <a className="skip-link" href="#main">
+        Skip to content
+      </a>
       <div className="layout-container">
         <MouseFollower />
         <NoiseBackground />
         <aside className="left-section">
           <Presentation />
           <Menu />
-          <Globe3D />
+          <Suspense fallback={null}>
+            <Globe3D />
+          </Suspense>
           <ContactIcons />
         </aside>
-        <div className="right-section">
+        <div className="right-section" id="main" tabIndex={-1}>
           <About />
           <ExperienceSection />
           <ProjectsSection />
